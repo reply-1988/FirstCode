bf3aeb7a66108b4f8f518a19b82df35eeb2622f9
reply-1988 <l>
2017-09-20 16:30:25 +0800

    测试版本1.0

---
+++ app/src/main/java/com/example/jingj/myapplication/Fruit.java
@@ -0,0 +1,24 @@
+package com.example.jingj.myapplication;
+
+/**
+ * Created by jingj on 2017/9/20.
+ */
+
+public class Fruit {
+
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
